ea993e67aa9c7e98dce02bdec7d8520a57b947fe
k3rn3l16 <k3rn3l16@outlook.com>
2017-06-19 13:44:22 -0700

    Add network connectivity check & display message for no internet connection

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,8 +17,11 @@ package com.example.android.quakereport;
 
 import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -60,6 +63,8 @@ public class EarthquakeActivity
 	 */
 	private TextView mEmptyStateView;
 
+	private View mProgressSpinner;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +86,17 @@ public class EarthquakeActivity
 		mEmptyStateView = (TextView) findViewById(R.id.empty_state_text_view);
 		earthquakeListView.setEmptyView(mEmptyStateView);
 
+		// get a reference to the progress bar
+		mProgressSpinner = findViewById(R.id.loading_spinner);
+
+		// Get a reference to the ConnectivityManager to check state of network connectivity
+		ConnectivityManager cm =
+				(ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+		// Get details on the currently active default data network
+		NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+		// Set boolean for whether the host system is connected to the internet or not
+		boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
 		// Set an item click listener on the ListView, which sends an intent to a web browser
 		// to open a website with more information about the selected earthquake.
 		earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -100,14 +116,23 @@ public class EarthquakeActivity
 				startActivity(websiteIntent);
 			}
 		});
+		if (isConnected) {
+			// Get a reference to the LoaderManager, in order to interact with loaders.
+			LoaderManager loaderManager = getLoaderManager();
 
-		// Get a reference to the LoaderManager, in order to interact with loaders.
-		LoaderManager loaderManager = getLoaderManager();
+			// Initialize the loader. Pass in the int ID constant defined above and pass in null for
+			// the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+			// because this activity implements the LoaderCallbacks interface).
+			loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+		} else {
+			// Otherwise, display error
+			// First, hide loading indicator so error message will be visible
+			mProgressSpinner.setVisibility(View.GONE);
 
-		// Initialize the loader. Pass in the int ID constant defined above and pass in null for
-		// the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-		// because this activity implements the LoaderCallbacks interface).
-		loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+			// Update empty state with no connection error message
+			mEmptyStateView.setText(R.string.no_internet);
+
+		}
 	}
 
 	@Override
@@ -117,11 +142,9 @@ public class EarthquakeActivity
 
 	@Override
 	public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> earthquakes) {
-		// get a reference to the progress bar
-		View progressBar = findViewById(R.id.loading_spinner);
 		// Hide progress bar
-		assert progressBar != null;
-		progressBar.setVisibility(View.GONE);
+		assert mProgressSpinner != null;
+		mProgressSpinner.setVisibility(View.GONE);
 
 		// Set empty state {@link TextView}
 		mEmptyStateView.setText(R.string.empty_state_text);
